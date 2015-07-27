a114fbb657e6bf3bf23c8a5f46bf2d662612de14
platky <platky1@gmail.com>
2015-07-27 19:45:27 -0400

    Initial Location implemented

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,11 @@
-function initialize() {
+var curLat=0;
+var curLon=0;
+
+
+function initialize(lon, lat) {
     console.log("initalizing map");
     var mapOptions = {
-        center: { lat: -34.397, lng: 150.644},
+        center: { lat: lat, lng: lon},
         zoom: 8
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -9,5 +13,29 @@ function initialize() {
 }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
 
+window.addEventListener('load', function() {
+    console.log("window loaded");
+    getLocation();
+});
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(updatePosition);
+    } else {
+        alert("GPS location not working");
+        console.log("alert working?");
+    }
+}
+
+function updatePosition(position){
+    curLat = position.coords.latitude;
+    curLon = position.coords.longitude;
+    initialize(curLon, curLat);
+}
+
+function showPosition(position){
+    alert("Latitude is " + position.coords.latitude +"\nLongitude is "+ position.coords.longitude);
+    console.log("Latitude is " + position.coords.latitude +" Longitude is "+ position.coords.longitude);
+}
