4d9ae7ee9e85e19de7be11f26e88cc87956bad89
platky <platky1@gmail.com>
2015-07-27 19:09:37 -0400

    Move to JS file

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+function initialize() {
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
